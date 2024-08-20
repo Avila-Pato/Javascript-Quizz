2aabce483321a5e0dceb4477718525e83980d065
Avila-Pato <p.avilaf1998@gmail.com>
2024-08-20 16:19:02 -0400

    agregando logica del comienzo del juego

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-
-
-import { Container, Stack, Typography } from '@mui/material'
-import './App.css'
-import { JavascriptLogo } from './Javascrit.tsx'
-import { Start } from './Start.tsx'
-import { useQuestionsStore } from './store/question.ts'
-import { Game } from './Game.tsx'
+// App.tsx
+import { useEffect, useState } from 'react';
+import { Container, Stack, Typography, CircularProgress } from '@mui/material';
+import './App.css';
+import { JavascriptLogo } from './Javascrit.tsx';
+import { Start } from './Start.tsx';
+import { useQuestionsStore } from './store/question.ts';
+import { Game } from './Game.tsx';
 
 function App() {
+  const { questions, fetchQuestions } = useQuestionsStore((state) => ({
+    questions: state.questions,
+    fetchQuestions: state.fetchQuestions,
+  }));
 
-// aqui el fect se esta ejecutando en el momento que se renderiza el juego
-const questions = useQuestionsStore(state => state.questions)
-console.log(questions)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadQuestions = async () => {
+      await fetchQuestions(10); // Llama a fetchQuestions con el límite deseado
+      setLoading(false);
+    };
+    loadQuestions();
+  }, [fetchQuestions]);
+
+  if (loading) {
+    return (
+      <Container maxWidth="sm">
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <CircularProgress />
+          <Typography variant='h6'>Cargando preguntas...</Typography>
+        </Stack>
+      </Container>
+    );
+  }
 
   return (
     <main>
       <Container maxWidth="sm">
-       <Stack direction='row' gap={2} alignItems='center' justifyContent= 'center'>
-      <JavascriptLogo />
-        <Typography variant='h2' component= 'h1' >
-       Javascript Quizz
-           </Typography>
-       </Stack>
-      {/* Aplicando logica para que empieze el juego */}
-       { questions.length === 0 && <Start />}
-      { questions.length > 0 && <Game />}
-
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <JavascriptLogo />
+          <Typography variant='h2' component='h1'>
+            Javascript Quizz
+          </Typography>
+        </Stack>
+        {/* Mostrar Start solo si questions.length es 0 después de la carga */}
+        {questions.length === 0 && <Start />}
+        {/* Mostrar Game si questions.length es mayor que 0 */}
+        {questions.length > 0 && <Game />}
       </Container>
     </main>
-  
-  )
+  );
 }
 
-export default App
+export default App;
